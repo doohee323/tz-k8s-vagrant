8cb8c80779bf62858d0e76db679921d192d38de6
dhong <dhong@fortinet.com>
2020-11-15 20:33:59 -0800

    shells

---
+++ scripts/shells/base.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x
+
+##################################################################
+# k8s base
+##################################################################
+
+sudo swapoff -a
+sudo sed -i '/swap/d' /etc/fstab
+sudo apt-get update
+sudo apt-get install -y docker.io apt-transport-https curl
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubeadm
+sudo mkdir -p /root/.ssh
+
---
+++ scripts/shells/master.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -x
+
+bash /vagrant/scripts/shells/base.sh
+
+##################################################################
+# k8s master
+##################################################################
+
+OUTPUT_FILE=/vagrant/join.sh
+rm -rf /vagrant/join.sh
+sudo kubeadm init --apiserver-advertise-address=10.0.0.10 --pod-network-cidr=10.244.0.0/16
+sudo kubeadm token create --print-join-command > /vagrant/join.sh
+chmod +x $OUTPUT_FILE
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl proxy --accept-hosts='^*' &
+
+cp /root/.kube/config /vagrant/config
+
+
