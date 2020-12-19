0273c23893b2179d540b9aaa0b50250f9e86701d
dhong <dhong@fortinet.com>
2020-12-19 15:31:13 -0800

    push latest to dockerhub

---
+++ tz-local/resource/test-app/python/push_latest.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#set -x
+
+cd /vagrant/projects/tz-py-crawler
+#vi Dockerfile
+#CMD [ "python", "/code/youtube/youtube/server.py" ]
+docker build -t tz-py-crawler .
+docker login -u="$USERNAME" -p="$PASSWD"
+docker tag tz-py-crawler:latest doohee323/tz-py-crawler:latest
+docker push doohee323/tz-py-crawler:latest
+
+exit 0
+
