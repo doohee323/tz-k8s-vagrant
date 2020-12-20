d14f4c5d4516547daeeb839d774c050f77c21e25
dhong <dhong@fortinet.com>
2020-12-19 16:38:48 -0800

    push latest to dockerhub

--- tz-local/resource/test-app/python/push_latest.sh
+++ tz-local/resource/test-app/python/push_latest.sh
@@ -6,6 +6,7 @@ USERNAME=$1
 PASSWD=$2
 
 cd /var/jenkins_home/workspace/tz-py-crawler_push
+#cd /vagrant/projects/tz-py-crawler
 
 if [[ ! -d 'projects/tz-py-crawler' ]]; then
   mkdir projects
@@ -17,8 +18,11 @@ cd /var/jenkins_home/workspace/tz-py-crawler_push/projects/tz-py-crawler
 
 #vi Dockerfile
 #CMD [ "python", "/code/youtube/youtube/server.py" ]
-docker build -t tz-py-crawler .
 docker login -u="$USERNAME" -p="$PASSWD"
+
+docker rmi tz-py-crawler
+docker build -t tz-py-crawler .
+docker image ls
 docker tag tz-py-crawler:latest doohee323/tz-py-crawler:latest
 docker push doohee323/tz-py-crawler:latest
 
