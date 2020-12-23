c455376537633c81ba3f25446b78f26c8a87e476
dhong <dhong@fortinet.com>
2020-12-22 23:50:17 -0800

    add rsync

--- tz-local/resource/test-app/python/sync_json.sh
+++ tz-local/resource/test-app/python/sync_json.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-set -x
+#k delete -f /vagrant/tz-local/resource/test-app/python/tz-py-syncJob.yaml
+#k apply -f /vagrant/tz-local/resource/test-app/python/tz-py-syncJob.yaml
+#k exec -it pod/tz-py-crawler-job-1608708060-w7gj7 -- sh
 
-/usr/bin/rsync -i doohee323 -avzh /mnt/result/ dhong@192.168.0.199:/Volumes/workspace/etc/tz-k8s-elk/data/
+#set -x
+
+mkdir -p /root/.ssh
+
+echo '
+Host 192.168.0.199
+  StrictHostKeyChecking   no
+  LogLevel                ERROR
+  UserKnownHostsFile      /dev/null
+  IdentitiesOnly yes
+  IdentityFile /mnt/doohee323
+' > /root/.ssh/config
+
+rsync -avzh -e ssh /mnt/result/ dhong@192.168.0.199:/Volumes/workspace/etc/tz-k8s-elk/data/
 
 exit 0
