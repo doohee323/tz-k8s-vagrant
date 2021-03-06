9734863b95536f3d933a4af68468b8f0d7c1ef70
dhong <dhong@fortinet.com>
2021-03-05 21:57:46 -0800

    dynamic nf storage

--- run.sh
+++ run.sh
@@ -70,9 +70,9 @@ vagrant snapshot save k8s-master k8s-master_master --force
 vagrant snapshot save node-1 node-1_master --force
 vagrant snapshot save node-2 node-2_master --force
 
-vagrant snapshot restore k8s-master k8s-master_python
-vagrant snapshot restore node-1 node-1_python
-vagrant snapshot restore node-2 node-2_python
+vagrant snapshot restore k8s-master k8s-master_latest
+vagrant snapshot restore node-1 node-1_latest
+vagrant snapshot restore node-2 node-2_latest
 
 vagrant snapshot restore k8s-master k8s-master_kafka
 vagrant snapshot restore node-1 node-1_kafka
