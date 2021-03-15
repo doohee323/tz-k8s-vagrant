1df50af33ca048b91bda17f6afef248b71531128
dhong <dhong@fortinet.com>
2021-03-14 20:13:10 -0700

    .

--- tz-local/resource/consul/install_nohelm.sh
+++ tz-local/resource/consul/install_nohelm.sh
@@ -3,6 +3,7 @@
 # https://caylent.com/hashicorp-vault-on-kubernetes
 #https://github.com/kainlite/vault-consul-tls/tree/master/consul
 #https://medium.com/swlh/consul-in-kubernetes-pushing-to-production-223506bbe8db
+git clone https://github.com/liejuntao001/consul-k8s-production.git
 
 #set -x
 shopt -s expand_aliases
@@ -10,15 +11,12 @@ alias k='kubectl'
 
 # install for test on host
 wget https://releases.hashicorp.com/consul/1.8.4/consul_1.8.4_linux_amd64.zip
-sudo apt-get install -y unzip jq
 unzip consul_1.8.4_linux_amd64.zip
 sudo mv consul /usr/local/bin/
 rm consul_1.8.4_linux_amd64.zip
 
-mkdir -p /vagrant/tz-local/resource/consul4
-cd /vagrant/tz-local/resource/consul4
-
-git clone https://github.com/liejuntao001/consul-k8s-production.git
+mkdir -p /vagrant/tz-local/resource/consul
+cd /vagrant/tz-local/resource/consul
 
 #Creating Certificates for Hashicorp Consul and Vault
 consul tls ca create
@@ -27,23 +25,17 @@ consul tls cert create -client
 mkdir certs
 mv *.pem certs
 
+cd consul-k8s-production
+
 #Hashicorp Consul
 #Create secret for the gossip protocol
 export GOSSIP_ENCRYPTION_KEY=$(consul keygen)
 
-cd consul-k8s-production
-
 #Step 1 bootstrap Consul cluster without ACL
 k create namespace consul
-kubectl apply -f consul/consul_service.yml
-kubectl apply -f consul/consul_serviceaccount.yml
-kubectl -n consul create configmap consul \
-  --from-file=config.json=consul/config/01_no_acl_config.json -o yaml --dry-run=client
-kubectl apply -f consul/01consul_statefulset_noacl.yml
+kubectl apply -f consul_service.yml
+kubectl apply -f consul_serviceaccount.yml
 
-#Step 2 Enable ACL without enforcing it
-kubectl -n consul create configmap consul --from-file=config.json=consul/config/02_acl_allow_config.json -o yaml --dry-run=client
-kubectl apply -f consul/02consul_statefulset_acl_allow.yml
-consul acl bootstrap
+kubectl -n consul create configmap consul --from-file=config.json=config/01_no_acl_config.json -o yaml --dry-run | kubectl replace -f -
 
-
+kubectl apply -f 01consul_statefulset_noacl.yml
