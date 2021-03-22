7293167b89f4b9e1c481ce69207a4461421feb69
Doohee Hong <doohee.hong@ejn.kr>
2021-03-21 17:02:33 -0700

    nexus1

--- tz-local/resource/nexus/helm.sh
+++ tz-local/resource/nexus/helm.sh
@@ -2,7 +2,8 @@
 
 helm repo add oteemocharts https://oteemo.github.io/charts
 helm search repo oteemocharts/sonatype-nexus
-helm install nexus oteemocharts/sonatype -f /vagrant/tz-local/resource/nexus/values.yaml -n sonatype-nexus
+helm repo update
+helm install nexus oteemocharts/sonatype-nexus -f /vagrant/tz-local/resource/nexus/values.yaml -n sonatype-nexus
 helm delete nexus -n sonatype-nexus
 
 #helm upgrade my-release stable/sonatype-nexus -f values.yaml -n sonatype-nexus --wait
