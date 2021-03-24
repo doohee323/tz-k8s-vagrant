68a4957d648fc09ebb94481425b8dc900ca21807
Doohee Hong <doohee.hong@ejn.kr>
2021-03-23 17:07:18 -0700

    nexus helm

---
+++ tz-local/resource/jenkins/helm/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#set -x
+shopt -s expand_aliases
+alias k='kubectl --kubeconfig ~/.kube/config'
+
+#cd /vagrant/tz-local/resource/nexus
+helm repo add jenkins https://charts.jenkins.io
+helm search repo jenkins
+
+helm list --all-namespaces -a
+
+k patch svc jenkins --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":31000}]' -n jenkins
+
+echo '
+##[ Jenkins ]##########################################################
+  - ID: admin
+  - Password:
+    kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
+
+
+#######################################################################
+' >> /vagrant/info
+cat /vagrant/info
