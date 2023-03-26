8a163f7c161b1d3653a480f0a19d1200955e4d3d
Doohee Hong <doohee.hong@ejn.kr>
2023-03-25 18:21:47 -0700

    .

--- tz-local/resource/jenkins/helm/install.sh
+++ tz-local/resource/jenkins/helm/install.sh
@@ -17,6 +17,7 @@ echo '
   - ID: admin
   - Password:
     kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
+    kubectl exec -it service/jenkins-1617143063 -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
 
 
 #######################################################################
