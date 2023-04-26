2050868608df261a2f73712fe4cc7f9c2029845d
Doohee Hong <doohee.hong@ejn.kr>
2023-04-26 12:19:26 -0700

    7

--- tz-local/resource/dynamic-provisioning/nfs/install.sh
+++ tz-local/resource/dynamic-provisioning/nfs/install.sh
@@ -6,26 +6,36 @@ alias k='kubectl --kubeconfig ~/.kube/config'
 # install NFS in k8s
 #https://github.com/kubernetes-csi/csi-driver-nfs/blob/master/deploy/example/nfs-provisioner/README.md
 
-# 1. Create a NFS provisioner
-k create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/nfs-provisioner/nfs-server.yaml
+apt update
+apt install -y nfs-server nfs-common
+mkdir /srv/nfs
+sudo chown nobody:nogroup /srv/nfs
+sudo chmod 0777 /srv/nfs
+cat << EOF >> /etc/exports
+/srv/nfs 192.168.0.0/24(rw,no_subtree_check,no_root_squash)
+EOF
+systemctl enable --now nfs-server
+exportfs -ar
 
-# 2. Install NFS CSI driver master version on a kubernetes cluster
-curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/install-driver.sh | bash -s master --
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --create-namespace \
+  --namespace nfs-provisioner \
+  --set nfs.server=192.168.0.20 \
+  --set nfs.path=/srv/nfs
 
-k -n kube-system get pod -o wide -l app=csi-nfs-controller
-k -n kube-system get pod -o wide -l app=csi-nfs-node
+## 1. Install NFS CSI driver master version on a kubernetes cluster
+#curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/install-driver.sh | bash -s master --
+#k -n kube-system get pod -o wide -l app=csi-nfs-controller
+#k -n kube-system get pod -o wide -l app=csi-nfs-node
 
-# may need to reboot vagrant reload
-k get all --all-namespaces | grep nfs
+## 3. Verifying a driver installation
+#k get csinodes \
+#-o jsonpath='{range .items[*]} {.metadata.name}{": "} {range .spec.drivers[*]} {.name}{"\n"} {end}{end}'
 
-# 3. Verifying a driver installation
-k get csinodes \
--o jsonpath='{range .items[*]} {.metadata.name}{": "} {range .spec.drivers[*]} {.name}{"\n"} {end}{end}'
-
-k create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/nfs-provisioner/nginx-pod.yaml
-k exec nginx-nfs-example -- bash -c "findmnt /var/www -o TARGET,SOURCE,FSTYPE"
-
-k delete -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/nfs-provisioner/nginx-pod.yaml
+#k create -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/nfs-provisioner/nginx-pod.yaml
+#k exec nginx-nfs-example -n default -- bash -c "findmnt /var/www -o TARGET,SOURCE,FSTYPE"
+#k delete -f https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/deploy/example/nfs-provisioner/nginx-pod.yaml
 
 cd /vagrant/tz-local/resource/dynamic-provisioning/nfs
 ###############################################################
