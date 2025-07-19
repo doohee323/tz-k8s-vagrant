e5d767d82d9951cdd05fd33050b83035e6e8eb44
dhong <dhong@fortinet.com>
2025-07-19 16:55:50 -0700

    airflow

--- tz-local/resource/airflow/run.sh
+++ tz-local/resource/airflow/run.sh
@@ -17,8 +17,6 @@ kubectl create ns ${NS}
 helm repo add apache-airflow https://airflow.apache.org
 helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace
 
-k8s_project='k8s-main-s'
-k8s_domain='argear.io'
 cp -Rf airflow-ingress.yaml airflow-ingress.yaml_bak
 sed -ie "s/k8s_project/${k8s_project}/g" airflow-ingress.yaml_bak
 sed -ie "s/k8s_domain/${k8s_domain}/g" airflow-ingress.yaml_bak
