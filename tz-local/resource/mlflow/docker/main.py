61fe1229ae2f607100ef21bcb74298845f4476f3
dhong <dhong@fortinet.com>
2025-07-20 23:32:22 -0700

    papermill

--- tz-local/resource/mlflow/docker/main.py
+++ tz-local/resource/mlflow/docker/main.py
@@ -1,46 +1,18 @@
 import os
 import mlflow
-import requests
 
 def main():
     print("Hello from converted notebook")
 
     print("----------------------------")
-    print(os.getenv("MLFLOW_AUTH_HEADER"))
     print(os.getenv("MLFLOW_TRACKING_URI"))
+    print(os.getenv("MLFLOW_TRACKING_USERNAME"))
+    print(os.getenv("MLFLOW_TRACKING_PASSWORD"))
     print("----------------------------")
-
-    headers = {
-        "Authorization": "Basic dXNlcjpZZ2JXTHV0TTYwRWg="
-    }
-
-    res = requests.get(
-        "https://mlflow.new-nation.church/api/2.0/mlflow/experiments/get-by-name",
-        params={"experiment_name": "Check localhost connection2"},
-        headers=headers
-    )
-
-    print(res.json())
-    print("----------------------------")
-
-    # 인증용 requests 세션 생성
-    session = requests.Session()
-    session.headers.update({
-        "Authorization": os.getenv("MLFLOW_AUTH_HEADER")  # ex: Basic dXNlcjpZZ2JXTHV0TTYwRWg=
-    })
-
-    # resp = requests.get(
-    #     f"{os.getenv('MLFLOW_TRACKING_URI')}/api/2.0/mlflow/experiments/list",
-    #     headers={"Authorization": os.getenv("MLFLOW_AUTH_HEADER")}
-    # )
-    # print(resp.status_code, resp.text)
-
-    # 세션을 MLflow에 주입 (비공식 방식이지만 현재까지 가장 안정적)
-    import mlflow.tracking
-    mlflow.tracking._tracking_service.utils._get_http_session = lambda: session
 
     # MLflow 서버 설정
     mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    # mlflow.set_tracking_uri("https://mlflow.new-nation.church")
 
     # 실험 이름 설정 (삭제된 이름이면 에러 발생하므로 주의)
     mlflow.set_experiment("Check localhost connection2")
